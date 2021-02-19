969cd5a152c702d33de16b3f213c6066cf8ea04d
Jordan Fly <73404951+MSnow-21@users.noreply.github.com>
2021-02-19 18:40:45 -0500

    Changed database naming. Created database and first table entry. Added gitignore.

---
+++ employeetrackerschema.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS employee_trackerDB;
+
+CREATE DATABASE employee_trackerDB;
+
+USE employee_trackerDB;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NULL
+    PRIMARY KEY (id)
+)
