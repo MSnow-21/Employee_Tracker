d501d491efbe9ce1a394ced26d0e3b9f54a72c0d
Jordan Fly <73404951+MSnow-21@users.noreply.github.com>
2021-02-19 18:47:16 -0500

    Added role table with column descriptions

--- employeetrackerschema.sql
+++ employeetrackerschema.sql
@@ -6,6 +6,14 @@ USE employee_trackerDB;
 
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(30) NULL
+    name VARCHAR(30) NULL,
     PRIMARY KEY (id)
-)
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NULL,
+    salary DECIMAL(10,4) NULL,
+    deparment_id INT NULL,
+    PRIMARY KEY (id)
+);
