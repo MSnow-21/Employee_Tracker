81c6aefc3273c85021a87193a29e9aae033e3476
Jordan Fly <73404951+MSnow-21@users.noreply.github.com>
2021-02-21 07:34:00 -0500

    Revised values for seeds sql. Created insert values for role and employee.

--- seeds.sql
+++ seeds.sql
@@ -1,16 +1,28 @@
 USE employee_trackerDB;
 
+/* Insert values into department */
 INSERT INTO department (name)
-VALUES("engineering");
+VALUES ("engineering");
 
-INSERT INTO role (first_name)
-VALUES("Mike");
+/* Insert values into role */
+INSERT INTO role (title)
+VALUES ("engineer")
 
-INSERT INTO role (last_name)
-VALUES("Smith");
+INSERT INTO role (salary)
+VALUES ("55000")
 
-INSERT INTO role (manager_id)
-VALUES(20);
+INSERT INTO role (deparment_id)
+VALUES (40)
 
+/*Insert values into employee*/
+
+INSERT INTO employee (first_name)
+VALUES ("Mike")
+
+INSERT INTO employee (last_name)
+VALUES ("Smith");
+
+INSERT INTO employee (manager_id)
+VALUES (20);
 
 
